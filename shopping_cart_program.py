36cd723b8cd1d90b4a8fcb52b6b07b54f4d950e4
Worm <mwormley008@gmail.com>
2023-02-24 20:52:42 -0600

    first commit

---
+++ shopping_cart_program.py
@@ -0,0 +1,18 @@
+# Build a shopping cart program with the following capabilities:
+
+# 1) Takes in an input
+# 2) Stores user input into a dictionary or list
+# 3) The User can add or delete items
+# 4) The User can see current shopping list
+# 5) The program Loops until user 'quits'
+# 6) Upon quiting the program, prints out a receipt of the items with total and quantity.
+
+
+shoppingcart = {}
+ask = input("Tell us what you want bruh. Type quit to quit.")
+def shoppingcart_function():
+    while ask != 'quit':
+            
+
+
+shoppingcart_function()
