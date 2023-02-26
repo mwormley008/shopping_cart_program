ebb476bf79891c0e3c2571ab0810ee674dbd8e9f
Worm <mwormley008@gmail.com>
2023-02-25 23:35:18 -0600

    Saturday night update I don't know how to reference ask for some reason it's giving me an unbound error

--- shopping_cart_program.py
+++ shopping_cart_program.py
@@ -7,12 +7,25 @@
 # 5) The program Loops until user 'quits'
 # 6) Upon quiting the program, prints out a receipt of the items with total and quantity.
 
+menu = [("apple", 5)]
+
 
 shoppingcart = {}
-ask = input("Tell us what you want bruh. Type quit to quit.")
+ask = input("Tell us what you want bruh. Type menu to display the menu. Type quit to quit. ")
+
 def shoppingcart_function():
     while ask != 'quit':
-            
+            if ask.lower() == "menu":
+                print(menu)
+                continue
+            item = ask
+            ask = input("How many do you want? Type quit to quit. ")
+            quantity = ask
+            ask = input("Would you like to continue y/n? ")
+            if ask.lower() == "y":
+                ask = input("Tell us what you want bruh. Type quit to quit. ")
+            else:
+                 return shoppingcart  
 
 
 shoppingcart_function()
